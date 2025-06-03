b3fd4f8a55db39cb75b5b972bebbc47f233bac28
Yasset Perez-Riverol <ypriverol@gmail.com>
2025-06-03 09:30:56 +0100

    minor changes

--- quantmsio/commands/utils/project.py
+++ quantmsio/commands/utils/project.py
@@ -1,81 +1,84 @@
 import click
-
+from pathlib import Path
 from quantmsio.core.project import check_directory
 from quantmsio.core.common import QUANTMSIO_VERSION
 
 
 @click.command(
-    "generate-pride-project-json",
-    short_help="Generate a json project file from original pride accession",
+    "create",
+    short_help="Generate a project file from original PRIDE accession",
 )
 @click.option(
     "--project-accession",
-    help="Project accession to generate the json file",
+    help="PRIDE project accession",
     required=True,
 )
 @click.option(
     "--sdrf-file",
-    help="the SDRF file needed to extract some of the metadata",
+    help="SDRF file path for metadata extraction",
     required=True,
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 @click.option(
     "--output-folder",
-    help="Folder where the Json file will be generated",
+    help="Output directory for generated files",
     required=True,
+    type=click.Path(file_okay=False, path_type=Path),
 )
 @click.option(
     "--software-name",
-    help="software name used to generate the data",
+    help="Software name used to generate the data",
     required=False,
 )
 @click.option(
     "--software-version",
-    help="software version used to generate the data",
+    help="Software version used to generate the data",
     required=False,
 )
 @click.option(
     "--delete-existing",
     help="Delete existing files in the output folder",
-    is_flag=False,
+    is_flag=True,
 )
-def generate_pride_project_json(
+def generate_pride_project_json_cmd(
     project_accession: str,
-    sdrf_file: str,
+    sdrf_file: Path,
     software_name: str,
     software_version: str,
-    output_folder: str,
+    output_folder: Path,
     delete_existing: bool,
 ):
+    """Generate a project file from the original PRIDE accession and SDRF file.
+    
+    The project file definition is available in the docs folder of this repository:
+    https://github.com/bigbio/quantms.io/blob/main/docs/PROJECT.md
+    
+    This command will generate the project file and attach the provided SDRF file.
+    
+    Args:
+        project_accession: PRIDE project accession
+        sdrf_file: SDRF file path for metadata extraction
+        software_name: Software name used to generate the data
+        software_version: Software version used to generate the data
+        output_folder: Output directory for generated files
+        delete_existing: Delete existing files in the output folder
     """
-    Generate a json project file from the original PX accession and SDRF file.
-    The Json file definition is available in the docs folder of
-    this repository https://github.com/bigbio/quantms.io/blob/main/docs/PROJECT.md. This command will generate
-    the Json and attach to the project files the SDRF file provided as input.
+    if not all([project_accession, sdrf_file, output_folder]):
+        raise click.UsageError("Please provide all required parameters")
 
-    :param project_accession: Project accession to generate the json file
-    :param sdrf_file: the SDRF file needed to extract some of the metadata
-    :param software_name: software name used to generate the data
-    :param software_version: software version used to generate the data
-    :param output_folder: Folder where the Json file will be generated
-    :param delete_existing: Delete existing files in the output folder
-    :return: none
-    """
-    if project_accession is None or sdrf_file is None or output_folder is None:
-        raise click.UsageError("Please provide all the required parameters")
+    project_handler = check_directory(str(output_folder), project_accession)
 
-    project_handler = check_directory(output_folder, project_accession)
-
-    # Populate the project handler with the metadata from Pride Archive and the SDRF file
+    # Populate the project handler with metadata from PRIDE Archive and SDRF file
     project_handler.populate_from_pride_archive()
-    project_handler.populate_from_sdrf(sdrf_file)
+    project_handler.populate_from_sdrf(str(sdrf_file))
     project_handler.add_quantms_version(quantmsio_version=QUANTMSIO_VERSION)
     project_handler.add_software_provider(
         sortware_name=software_name, sortware_version=software_version
     )
-    project_path = output_folder + "/" + "project.json"
+    project_path = str(output_folder / "project.json")
     project_handler.add_sdrf_file(
-        sdrf_file_path=sdrf_file,
-        output_folder=output_folder,
+        sdrf_file_path=str(sdrf_file),
+        output_folder=str(output_folder),
         delete_existing=delete_existing,
     )
     project_handler.save_updated_project_info(output_file_name=project_path)
